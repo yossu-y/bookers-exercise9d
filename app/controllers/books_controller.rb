e04a36344dec42aa9786d23ac776e19e2b74ed13
yossu-y <yangyuub@gmail.com>
2022-05-28 05:37:16 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
+  def show
+    @book = Book.find(params[:id])
+    @book_comment = BookComment.new
+  end
+
+  def index
+    @books = Book.all.order(params[:sort])
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    tag_list = params[:book][:tag_name].split(',')
+    if @book.save
+      @book.save_tags(tag_list)
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :rate)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
+  end
+end
---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,16 @@
+class SearchesController < ApplicationController
+  before_action :authenticate_user!
+
+	def search
+		@model = params[:model]
+		@content = params[:content]
+		@method = params[:method]
+		if @model == 'user'
+			@records = User.search_for(@content, @method)
+		elsif @model == 'book'
+			@records = Book.search_for(@content, @method)
+		elsif @model == 'tag'
+			@records = Tag.search_books_for(@content, @method)
+		end
+	end
+end
